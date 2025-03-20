d36cc05d339e494b0f0a871a7ff6c98295462e75
Danilo Osorio <odanilo38@gmail.com>
2025-03-19 20:26:19 -0500

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,32 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Danilo Osorio on 19/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NotasApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
